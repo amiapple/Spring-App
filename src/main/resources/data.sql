6ca726473910140456e33bc12c4f057d0ed98d26
takabeami <takabeami@192.168.9.106>
2022-02-22 07:08:41 +0900

    コミット

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO inquiry(name, email, contents, created)
+VALUES('鈴木', 'suzuki@example.com', 'Hello', '2021-12-10 10:10:10');
+INSERT INTO inquiry(name, email, contents, created)
+VALUES('佐藤', 'sato@example.com', 'GoodMorning', '2021-12-15 11:11:11');
+INSERT INTO inquiry(name, email, contents, created)
+VALUES('山田', 'yamada@example.com', 'GoodEvening', '2021-12-15 15:15:15');
+
+INSERT INTO survey(age, satisfaction, comment, created)
+VALUES(30, 5, 'good!', '2021-12-20 12:12:12');
+INSERT INTO survey(age, satisfaction, comment, created)
+VALUES(40, 5, 'good', '2021-12-21 13:13:13');
+INSERT INTO survey(age, satisfaction, comment, created)
+VALUES(50, 5, 'good', '2021-12-22 14:14:14');
